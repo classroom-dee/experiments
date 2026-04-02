b630f13a21851600e7c1f0ae402e322cf77f1d89
Dee <123602432+boolYikes@users.noreply.github.com>
2026-04-02 18:15:33 +0000

    chore: update neo4j poc

--- neo4j-stuff/neo4j_starter.sh
+++ neo4j-stuff/neo4j_starter.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
 set -a
-source .env
+source $SCRIPT_DIR/.env
 set +a
 
 name=neo4j-test
 
-docker run -d --rm \
+docker run -d \
   --name $name \
   -p 7474:7474 -p 7687:7687 \
   -e NEO4J_AUTH=$NEO4J_USER/$NEO4J_PW \
   -v neo4j-test-volume:/data \
-  neo4j:5.26.9-community
+  neo4j:5.26.24-community-ubi10
 
